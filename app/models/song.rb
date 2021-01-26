0a06eb8a51318180f51a143b06c6e35008974a2f
github username <scott.gloyna01@gmail.com>
2021-01-26 16:24:03 -0600

    commit tree a46dd32bf289e809235d9208b5e60785a3498add

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
@@ -10,5 +11,11 @@ class Song < ActiveRecord::Base
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
 
+    if Artist.find_by_name("Drake")
+      Artist.find_by_name("Drake").songs.push(self)
+    else
+      drake = Artist.create(name: "Drake")
+      drake.songs.push(self)
+    end
   end
 end
